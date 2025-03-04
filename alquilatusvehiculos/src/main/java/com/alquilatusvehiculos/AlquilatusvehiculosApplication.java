0bfcaac9364195100c2794dea0db6ca4321cccef
paulacmorales <paulacmorales@uoc.edu>
2025-03-04 21:20:59 +0100

    Inicialización del repositorio

---
+++ alquilatusvehiculos/src/main/java/com/alquilatusvehiculos/AlquilatusvehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.alquilatusvehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilatusvehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilatusvehiculosApplication.class, args);
+	}
+
+}
---
+++ alquilatusvehiculos/src/test/java/com/alquilatusvehiculos/AlquilatusvehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alquilatusvehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilatusvehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
