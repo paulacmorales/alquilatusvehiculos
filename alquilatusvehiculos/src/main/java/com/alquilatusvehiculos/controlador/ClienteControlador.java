3b80eb3f4ba440e4e539f391b2c4d85b1072d5a4
Bassleader777 <gpascualsar@uoc.edu>
2025-04-05 20:52:20 +0200

    Complet

---
+++ alquilatusvehiculos/src/main/java/com/alquilatusvehiculos/controlador/ClienteControlador.java
@@ -0,0 +1,5 @@
+package com.alquilatusvehiculos.controlador;
+
+public class ClienteControlador {
+    
+}
---
+++ alquilatusvehiculos/src/main/java/com/alquilatusvehiculos/repositorio/VehiculoControlador.java
@@ -0,0 +1,5 @@
+package com.alquilatusvehiculos.repositorio;
+
+public class VehiculoControlador {
+    
+}
