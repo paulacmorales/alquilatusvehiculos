7739853a0999202fbba100aebe2cb34843e0d51a
paulacmorales <paulacmorales@uoc.edu>
2025-05-12 21:08:09 +0200

    Generar Rest Controllers

---
+++ alquilatusvehiculos/src/main/java/com/alquilatusvehiculos/controlador/api/AlquilerRestController.java
@@ -0,0 +1,21 @@
+package com.alquilatusvehiculos.controlador.api;
+
+import com.alquilatusvehiculos.modelo.Alquiler;
+import com.alquilatusvehiculos.Repositorios.AlquilerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/alquileres")
+public class AlquilerRestController {
+
+    @Autowired
+    private AlquilerRepository alquilerRepository;
+
+    @GetMapping
+    public List<Alquiler> getAllAlquileres() {
+        return alquilerRepository.findAll();
+    }
+}
---
+++ alquilatusvehiculos/src/main/java/com/alquilatusvehiculos/controlador/api/ClienteRestController.java
@@ -0,0 +1,26 @@
+package com.alquilatusvehiculos.controlador.api;
+
+import com.alquilatusvehiculos.modelo.Cliente;
+import com.alquilatusvehiculos.Repositorios.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteRestController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @GetMapping
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente getClienteById(@PathVariable Long id) {
+        return clienteRepository.findById(id).orElse(null);
+    }
+}
